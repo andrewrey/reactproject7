754e577b5073418383bf272bb43879abd7050e41
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-04 20:17:22 -0700

    created guestName component

--- src/Guest.js
+++ src/Guest.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import GuestName from "./GuestName";
 
 const Guest = ({
   name,
@@ -10,7 +11,7 @@ const Guest = ({
 }) => {
   return (
     <li>
-      {isEditing ? <input type="text" /> : <span>{name}</span>}
+      <GuestName isEditing={isEditing}>{name}</GuestName>
       <label>
         <input type="checkbox" checked={isConfirmed} onClick={handleConfirm} />{" "}
         Confirmed
@@ -25,6 +26,8 @@ Guest.propTypes = {
   name: PropTypes.string.isRequired,
   isConfirmed: PropTypes.bool.isRequired,
   handleConfirm: PropTypes.func.isRequired,
+  toggleEditingAt: PropTypes.func.isRequired,
+  isEditing: PropTypes.bool.isRequired,
 };
 
 export default Guest;
